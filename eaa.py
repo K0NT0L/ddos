1dcadf1ad382e72b371ae1b4abdc6b87d2fe0f01
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-01-24 23:13:42 +0700

    Create eaa.py

---
+++ eaa.py
@@ -0,0 +1,34 @@
+ #!/usr/bin/python
+ 
+import os
+import sys
+
+os.system("clear")
+os.system("figlet Welcome | lolcat")
+print ("\033[1;32mLogin Dulu ea")
+print ("\033[1;31;1mPm StarFuckTak'-' Dikalau anda bosan wkwk")
+username='FUCKKING'      
+password='240918'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("username : ")
+	if uname == username:
+		pwd = raw_input("password : ")
+
+		if pwd == password:
+			print "\n\033[1;34mSukses!!", 
+			exit()
+
+		else:
+			print "\n\033[1;36mMaafkeun Aing Anda Harus Mengulang !!!\033[00m"
+			print "Back Login\n"
+			restart()
+try:
+	main()
+except KeyboardInterrupt:
+	system('clear')
+	restart()
